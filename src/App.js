3725de222453e3f19b38e0fb7f564802bbab39a2
Breno <brenogpa@gmail.com>
2023-11-23 17:22:14 -0300

    implemented dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
     <div className="App">
       <Header />
       <ListarTarefa />
     </div>
+  </ThemeProvider>
+
   );
 }
 
